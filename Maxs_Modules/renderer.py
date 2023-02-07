094a923f60363c0808985a2f33587d6b98d0829a
Max Tyson <63089527+maxtyson123@users.noreply.github.com>
2023-02-07 19:43:52 +1300

    Renderer as a module

---
+++ Maxs_Modules/renderer.py
@@ -0,0 +1,98 @@
+import os
+from Maxs_Modules.tools import validate_user_input_number, error
+
+console_width = 30
+divider_symbol = "#"
+divider = divider_symbol * console_width
+
+
+def text_in_divider(item_to_print, auto_truncate=True):
+
+    # If the text is longer than the console width then truncate it
+    if len(item_to_print) > console_width and auto_truncate:
+        item_to_print = item_to_print[:console_width - 2]  # Truncate the text to fit the console width
+
+    width_left = console_width - len(item_to_print) - 2  # The length of the text, minus console width, minus 2 for the border
+    return divider_symbol + item_to_print + " " * width_left + divider_symbol
+
+
+def show_menu(menu_items):
+    print(divider)
+
+    # Loop through all the items in the menu
+    for x in range(len(menu_items)):
+        item_to_print = " [" + str(x) + "]" + " " + menu_items[x]
+        print(text_in_divider(item_to_print))
+    print(divider)
+
+
+def show_menu_double(menu_items):
+    print(divider)
+
+    # Loop through all the items in the menu
+    for x in range(len(menu_items[0])):
+        final_item_to_print = ""
+
+        # Create the two items to print
+        item_to_print_1 = " [" + str(x) + "]" + " " + menu_items[0][x]
+        item_to_print_2 = "(" + menu_items[1][x] + ") "
+
+        # Truncate the text if it is too long
+        if len(item_to_print_1) > console_width/2:
+            item_to_print = item_to_print_1[:console_width/2 - 2]  # Truncate the text to fit half the console width
+
+        if len(item_to_print_2) > console_width / 2:
+            item_to_print = item_to_print_1[:console_width / 2 - 2]  # Truncate the text to fit half the console width
+
+        # Spacing inbetween the two items (similar to how it is done in "text_in_divider()" function)
+        width_left = console_width - len(item_to_print_1) - len(item_to_print_2) - 2  # The length of the text, minus console width, minus 2 for the border
+        spacing = " " * width_left
+
+        # Combine the two items
+        final_item_to_print = divider_symbol + item_to_print_1 + spacing + item_to_print_2 + divider_symbol
+        print(final_item_to_print)
+
+    print(divider)
+
+
+class Menu:
+    # Note for future, the print should be changed to a render() function that allows for the menu to be rendered in
+    # different ways (CLI, GUI)
+
+    title = "None"
+    items = []
+    user_input = "undefined"
+    multi_dimensional = None
+
+    def __init__(self, title, items, multi_dimensional=False):
+        self.title = title
+        self.items = items
+        self.multi_dimensional = multi_dimensional
+
+    def show(self):
+
+        # Clear the screen
+        os.system("cls")
+
+        # Print the menu
+        print(divider)
+        print(text_in_divider(" " + self.title))
+        show_menu(self.items)
+
+        # Calculate the possible options
+        options = [*range(len(self.items))]
+
+        # Get the user input and validate it
+        invalid_input = True
+        user_input = "null"
+        while invalid_input:
+            user_input = input("Choose an option (" + str(options[0]) + "-" + str(
+                options[len(options) - 1]) + ") :")  # Get the user input
+            if validate_user_input_number(user_input):  # Check if the user inputted a number
+                if int(user_input) in options:  # Check if the user inputted a valid option
+                    invalid_input = False  # If it is valid then stop the loop
+                else:
+                    error("Invalid input, please enter a valid option")  # If it is not valid then print error
+
+        # Store the input
+        self.user_input = self.items[int(user_input)]
--- main.py
+++ main.py
@@ -4,11 +4,9 @@ import os
 
 from Maxs_Modules.files import SaveFile
 from Maxs_Modules.tools import debug, error
+from Maxs_Modules.renderer import Menu
 
 #              Variables       #
-console_width = 30
-divider_symbol = "#"
-divider = divider_symbol * console_width
 data_folder = "UserData/"
 
 
@@ -29,31 +27,6 @@ def handle_args(args):
     print("Quiz running with args:")
     for arg in args:
         print(arg)
-
-
-def validate_user_input_number(user_input):
-    try:
-        int(user_input)  # try to convert to int
-        return True
-    except ValueError:  # if it cant the return error
-        error("Invalid input, please enter a number")
-        return False
-
-
-def text_in_divider(item_to_print):
-    width_left = console_width - len(
-        item_to_print) - 2  # The length of the text, minus console width, minus 2 for the border
-    print(divider_symbol + item_to_print + " " * width_left + divider_symbol)
-
-
-def show_menu(menu_items):
-    print(divider)
-
-    # Loop through all the items in the menu
-    for x in range(len(menu_items)):
-        item_to_print = " [" + str(x) + "]" + " " + menu_items[x]
-        text_in_divider(item_to_print)
-    print(divider)
 
 
 # - - - - - - - Classes - - - - - - -#
@@ -77,47 +50,6 @@ class UserSettings(SaveFile):
 
         # Call the super class save function
         super().save()
-
-
-class Menu:
-    # Note for future, the print should be changed to a render() function that allows for the menu to be rendered in
-    # different ways (CLI, GUI)
-
-    title = "None"
-    items = []
-    user_input = "undefined"
-
-    def __init__(self, title, items):
-        self.title = title
-        self.items = items
-
-    def show(self):
-
-        # Clear the screen
-        os.system("cls")
-
-        # Print the menu
-        print(divider)
-        text_in_divider(" " + self.title)
-        show_menu(self.items)
-
-        # Calculate the possible options
-        options = [*range(len(self.items))]
-
-        # Get the user input and validate it
-        invalid_input = True
-        user_input = "null"
-        while invalid_input:
-            user_input = input("Choose an option (" + str(options[0]) + "-" + str(
-                options[len(options) - 1]) + ") :")  # Get the user input
-            if validate_user_input_number(user_input):  # Check if the user inputted a number
-                if int(user_input) in options:  # Check if the user inputted a valid option
-                    invalid_input = False  # If it is valid then stop the loop
-                else:
-                    error("Invalid input, please enter a valid option")  # If it is not valid then print error
-
-        # Store the input
-        self.user_input = self.items[int(user_input)]
 
 
 # - - - - - - - Functions - - - - - - -#
